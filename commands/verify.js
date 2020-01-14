6d8e435cd62d59435f0fda08bac4dd190065d06e
BrightnessTM <khanolympics@gmail.com>
2020-01-13 21:32:26 -0500

    Verify improvements

--- commands/verify.js
+++ commands/verify.js
@@ -56,21 +56,33 @@ module.exports = {
 		if (qServerDB && qServerDB.blocked) posArr.push(":no_entry_sign: Blacklisted on this server");
 		if (client.guilds.get(config.main_guild) && client.guilds.get(config.main_guild).available && client.guilds.get(config.main_guild).roles.get("614084573139173389").members.get(id)) globalPosArr.push("<:canary:621530343081508899> Suggester Canary Program");
 
+		let hasAcks = false;
 		let permissionLevel = await checkPermissions(message.guild.members.get(id), client);
 		let embed = new Discord.RichEmbed()
 			.setAuthor(client.users.get(id).tag, client.users.get(id).displayAvatarURL)
 			.setColor(config.colors.default)
 			.setFooter(`Permission Level: ${permissionLevel}`);
-		if (globalPosArr.length > 0) embed.addField("Global Acknowledgements", `${globalPosArr.join("\n")}`);
-		if (posArr.length > 0) embed.addField("Server Acknowledgements", `${posArr.join("\n")}`);
-		qUserDB.ack ? embed.setDescription(qUserDB.ack) : embed.setDescription("This user has no acknowledgements");
-
+		if (globalPosArr.length > 0) {
+			embed.addField("Global Acknowledgements", `${globalPosArr.join("\n")}`);
+			hasAcks = true;
+		}
+		if (posArr.length > 0) {
+			embed.addField("Server Acknowledgements", `${posArr.join("\n")}`);
+			hasAcks = true;
+		}
 		if (qUserDB.beans) {
 			let beans = qUserDB.beans;
-			embed.addField("Received Bean Statistics <:bean:657650134502604811>", `<:bean:657650134502604811> ${beans.received.bean} beans\n<:hyperbean:666099809668694066> ${beans.received.megabean} megabeans\n<:nukebean:666102191895085087> ${beans.received.nukebean} nukebeans`)
-				.addField("Sent Bean Statistics <:bean:657650134502604811>", `<:bean:657650134502604811> ${beans.sent.bean} beans\n<:hyperbean:666099809668694066> ${beans.sent.megabean} megabeans\n<:nukebean:666102191895085087> ${beans.sent.nukebean} nukebeans`);
+			let beanCountTotal = beans.sent.bean+beans.sent.megabean+beans.sent.nukebean+beans.received.bean+beans.received.megabean+beans.received.nukebean;
+			if (beanCountTotal > 0) {
+				embed.addField("Received Bean Statistics <:bean:657650134502604811>", `<:bean:657650134502604811> ${beans.received.bean} beans\n<:hyperbean:666099809668694066> ${beans.received.megabean} megabeans\n<:nukebean:666102191895085087> ${beans.received.nukebean} nukebeans`)
+					.addField("Sent Bean Statistics <:bean:657650134502604811>", `<:bean:657650134502604811> ${beans.sent.bean} beans\n<:hyperbean:666099809668694066> ${beans.sent.megabean} megabeans\n<:nukebean:666102191895085087> ${beans.sent.nukebean} nukebeans`);
+				hasAcks = true;
+			}
 		}
 
+		if (qUserDB.ack) embed.setDescription(qUserDB.ack);
+		else if (!hasAcks) embed.setDescription("This user has no acknowledgements");
+		
 		return message.channel.send(embed);
 	}
 };
