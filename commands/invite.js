2f9a95dfb5351da70190625e2ce9fe6f7a02fb4d
BrightnessTM <khanolympics@gmail.com>
2020-01-16 08:25:07 -0500

    Invite command

--- commands/invite.js
+++ commands/invite.js
@@ -1,4 +1,5 @@
 const config = require("../config.json");
+const { checkPermissions } = require("../coreFunctions");
 module.exports = {
 	controls: {
 		permission: 10,
@@ -8,28 +9,23 @@ module.exports = {
 		docs: "all/invite",
 		permissions: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS", "USE_EXTERNAL_EMOJIS"]
 	},
-	do: (message, client, args, Discord) => {
-		if (config.release === "stable") return message.reply(`You can invite Suggester to your server with this link: <https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=604367937>`);
+	do: async (message, client, args, Discord) => {
+		const url = "<https://discordapp.com/oauth2/authorize?client_id=[ID]&scope=bot&permissions=604367937>";
+		let stableId = "564426594144354315";
+		if (config.release === "stable") return message.reply(`You can invite Suggester to your server with this link: ${url.replace("[ID]", client.user.id)}`);
 		if (config.release === "canary") {
-			if (client.guilds.get(config.main_guild) && client.guilds.get(config.main_guild).available && client.guilds.get(config.main_guild).roles.get("614084573139173389").members.get(message.author.id)) {
-				let embed = new Discord.RichEmbed()
-					.setDescription("<:canary:621530343081508899> You are a member of the Canary program!")
-					.addField("Invite", `You can invite the Canary bot to your server with this link: <https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=604367937>`)
-					.setColor(config.default_color);
-				return message.reply("This instance of Suggester is the **Canary** version. You must apply to be a part of the Canary program on the main server.\nYou can invite the **public** version of Suggester at <https://discordapp.com/oauth2/authorize?client_id=564426594144354315&scope=bot&permissions=604367937>.", embed);
+			let userPermission = await checkPermissions(message.member, client);
+			if (client.guilds.get(config.main_guild) && client.guilds.get(config.main_guild).available && client.guilds.get(config.main_guild).roles.get("614084573139173389").members.get(message.author.id) || userPermission <= 1) {
+				return message.channel.send(`You can invite the Canary bot to your server with this link: ${url.replace("[ID]", client.user.id)}`);
 			} else {
-				return message.reply("This instance of Suggester is the **Canary** version. You must apply to be a part of the Canary program on the main server.\nYou can invite the **public** version of Suggester at <https://discordapp.com/oauth2/authorize?client_id=564426594144354315&scope=bot&permissions=604367937>.");
+				return message.channel.send(`This instance of Suggester is the **Canary** version. You must apply to be a part of the Canary program on the main server.\nYou can invite the **public** version of Suggester at ${url.replace("[ID]", stableId)}`);
 			}
 		}
 		if (config.release === "special") {
 			if (config.developer.includes(message.author.id)) {
-				let embed = new Discord.RichEmbed()
-					.setDescription("<:suggester:621530308592009242> You are a global administrator!")
-					.addField("Invite", `You can invite this instance of the bot to your server with this link: <https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=604367937>`)
-					.setColor(config.default_color);
-				return message.reply("This instance of Suggester is a private version made specifically for a server.\nYou can invite the **public** version of Suggester at <https://discordapp.com/oauth2/authorize?client_id=564426594144354315&scope=bot&permissions=604367937>.", embed);
+				return message.channel.send(`You can invite this instance of the bot to your server with this link: ${url.replace("[ID]", client.user.id)}`);
 			}
-			return message.reply("This instance of Suggester is a private version made specifically for a server.\nYou can invite the **public** version of Suggester at <https://discordapp.com/oauth2/authorize?client_id=564426594144354315&scope=bot&permissions=604367937>.");
+			return message.channel.send(`This instance of Suggester is a private version.\nYou can invite the **public** version of Suggester at ${url.replace("[ID]", stableId)}`);
 		}
 	}
 };
