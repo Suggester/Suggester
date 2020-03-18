93990a0670cd4c930aadc93265c74d0590aa1bd7
BrightnessTM <khanolympics@gmail.com>
2020-03-18 14:32:37 -0400

    Hotfix guildDelete (and create)

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,7 +4,7 @@ module.exports = async (Discord, client, guild) => {
 	let qServerDB = await dbQuery("Server", { id: guild.id });
 	if (qServerDB && qServerDB.blocked) {
 		await guild.leave();
-		return guildLog(`:no_entry: I was added to blacklisted guild **${guild.name}** (${guild.id}) and left`, client);
+		return guildLog(`:no_entry: I was added to blacklisted guild **${guild.name ? guild.name : "Name Unknown"}** (\`${guild.id ? guild.id : "ID Unknown"}\`) and left`, client);
 	}
 
 	let enforceWhitelist = [
@@ -14,8 +14,8 @@ module.exports = async (Discord, client, guild) => {
 	];
 	if ((enforceWhitelist.includes(release)) && !qServerDB.whitelist) {
 		await guild.leave();
-		return guildLog(`:no_entry: I was added to non-whitelisted guild **${guild.name}** (${guild.id}) and left`, client);
+		return guildLog(`:no_entry: I was added to non-whitelisted guild **${guild.name ? guild.name : "Name Unknown"}** (\`${guild.id ? guild.id : "ID Unknown"}\`) and left`, client);
 	}
 
-	await guildLog(`:inbox_tray: New Guild: **${guild.name}** (${guild.id})\n>>> **Owner:** ${guild.owner.user.tag}\n**Member Count:** ${guild.memberCount}`, client);
+	await guildLog(`:inbox_tray: New Guild: **${guild.name ? guild.name : "Name Unknown"}** (\`${guild.id ? guild.id : "ID Unknown"}\`)\n>>> **Owner:** ${guild.owner && guild.owner.user ? `${guild.owner.user.tag} (\`${guild.owner.id}\`)` : "Owner Tag Unknown"}\n**Member Count:** ${guild.memberCount ? guild.memberCount : "Member Count Unknown"}`, client);
 };
