5b74892830286c3f4abd31f1d5a41b89bf654b6e
BrightnessTM <khanolympics@gmail.com>
2020-03-23 13:48:16 -0400

    DB improvements

--- commands/admin/db.js
+++ commands/admin/db.js
@@ -1,28 +1,46 @@
-const { dbQueryNoNew } = require("../../coreFunctions.js");
+const { dbQueryNoNew, dbModify } = require("../../coreFunctions.js");
 const { emoji, colors } = require("../../config.json");
 module.exports = {
 	controls: {
 		name: "db",
 		permission: 0,
 		aliases: ["query"],
-		usage: "db <collection> <query field> <query value>",
+		usage: "db <query|modify> <collection> <query field> <query value> (modify:field) (modify:value)",
 		description: "Gets a database entry",
 		enabled: true,
 		permissions: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS", "USE_EXTERNAL_EMOJIS"]
 	},
 	do: async (message, client, args, Discord) => {
-		if (!args[0] || !args[1] || !args[2]) return message.channel.send(`<:${emoji.x}> You must specify a collection name, query field, and query value.`);
-		let collection = args[0];
-		let field = args[1];
-		let value = args[2];
+		if (!args[0] || !args[1] || !args[2] || !args[3]) return message.channel.send(`<:${emoji.x}> You must specify whether to query or modify, a collection name, query field, and query value.`);
+		let collection = args[1];
+		let field = args[2];
+		let value = args[3];
 		let query = {};
 		query[field] = value;
 		let result = await dbQueryNoNew(collection, query);
 		if (result === 0) return message.channel.send(`Collection \`${collection}\` is an invalid collection.`);
+		let modified = false;
+		let modifyField;
+		let modifyValue;
+		let oldValue;
+		if (args[0].toLowerCase() === "modify") {
+			if (!args[4] || !args[5]) return message.channel.send(`<:${emoji.x}> You must specify modification parameters!`);
+			modifyField = args[4];
+			oldValue = eval(`result.${modifyField}`);
+			modifyValue = args[5];
+			eval(`result.${modifyField} = ${modifyValue}`);
+			await dbModify(collection, query, result);
+			modified = true;
+		}
 		let embed = new Discord.MessageEmbed()
-			.setTitle("Database Query")
-			.setDescription(`**Collection:** ${collection}\n**Query:** ${JSON.stringify(query)}`)
-			.addField("Result", result ? `\`\`\`${result.toString().substr(0, 1020)}\`\`\`` : "No Result Found")
+			.setTitle(`Database ${modified ? "Modified": "Query"}`)
+			.setDescription(`**Collection:** ${collection}\n**Query:** ${JSON.stringify(query)}`);
+
+		if (modified) {
+			embed.addField("Modified", `**Field:** ${modifyField}\n**Old Value:** ${oldValue}\n**New Value:** ${modifyValue}`);
+		}
+
+		embed.addField("Result", result ? `\`\`\`${result.toString().substr(0, 1020)}\`\`\`` : "No Result Found")
 			.setColor(result ? colors.default : "#ff0000");
 		return message.channel.send(embed);
 	}
