82acd20c98da4c8c165be9237ab4fdc0f3341019
BrightnessTM <khanolympics@gmail.com>
2020-04-15 08:36:47 -0400

    Check for Manage Messages when configuring cleancommands

--- commands/server admin/config.js
+++ commands/server admin/config.js
@@ -615,6 +615,7 @@ module.exports = {
 			case "enable":
 			case "on": {
 				if (!qServerDB.config.clean_suggestion_command) {
+					if (!message.guild.me.permissions.has("MANAGE_MESSAGES")) return message.channel.send(`<:${emoji.x}> Auto-cleaning of suggestion commands requires the bot have the **Manage Messages** permission. Please give the bot this permission and try again.`);
 					qServerDB.config.clean_suggestion_command = true;
 					await dbModify("Server", {id: message.guild.id}, qServerDB);
 					return message.channel.send(`<:${emoji.check}> Enabled auto-cleaning of suggestion commands.`);
@@ -629,6 +630,7 @@ module.exports = {
 				} else return message.channel.send(`<:${emoji.x}> Auto-cleaning of suggestion commands is already disabled!`);
 			}
 			case "toggle":
+				if (!qServerDB.config.clean_suggestion_command && !message.guild.me.permissions.has("MANAGE_MESSAGES")) return message.channel.send(`<:${emoji.x}> Auto-cleaning of suggestion commands requires the bot have the **Manage Messages** permission. Please give the bot this permission and try again.`)
 				qServerDB.config.clean_suggestion_command = !qServerDB.config.clean_suggestion_command;
 				await dbModify("Server", {id: message.guild.id}, qServerDB);
 				return message.channel.send(`<:${emoji.check}> ${qServerDB.config.clean_suggestion_command ? "Enabled" : "Disabled"} auto-cleaning of suggestion commands.`);
