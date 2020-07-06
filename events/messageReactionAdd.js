459ba367bc6167c2677f5d2c94683e6815810dd8
BrightnessTM <khanolympics@gmail.com>
2020-07-06 16:45:48 -0400

    Fix bug with reaction handling

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,11 +1,11 @@
-const { dbQuery } = require("../utils/db");
+const { dbQueryNoNew } = require("../utils/db");
 const { editFeedMessage } = require("../utils/actions");
 module.exports = async (Discord, client, messageReaction, user) => {
 	const nodeEmoji = require("node-emoji");
 	if (messageReaction.message.partial) messageReaction.message = await messageReaction.message.fetch();
 	let db = await messageReaction.message.guild.db;
 
-	let suggestion = await dbQuery("Suggestion", { id: messageReaction.message.guild.id, messageId: messageReaction.message.id });
+	let suggestion = await dbQueryNoNew("Suggestion", { id: messageReaction.message.guild.id, messageId: messageReaction.message.id });
 	if (!suggestion) return;
 
 	let emotes = [suggestion.emojis.up.match(/a?:?.+:(\d+)/) ? suggestion.emojis.up.match(/a?:?.+:(\d+)/)[1] : suggestion.emojis.up, suggestion.emojis.mid.match(/a?:?.+:(\d+)/) ? suggestion.emojis.mid.match(/a?:?.+:(\d+)/)[1] : suggestion.emojis.mid, suggestion.emojis.down.match(/a?:?.+:(\d+)/) ? suggestion.emojis.down.match(/a?:?.+:(\d+)/)[1] : suggestion.emojis.down];
