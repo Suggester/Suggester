78823bf27ac34f64e395e966259234a897533022
Samsamson33 <samsamson33.0913@gmail.com>
2020-09-17 23:54:38 -0400

    Add `.unfollow auto[matic]`

--- commands/other/unfollow.js
+++ commands/other/unfollow.js
@@ -1,4 +1,4 @@
-const { dbQuery } = require("../../utils/db");
+const { dbModify, dbQuery } = require("../../utils/db");
 const { checkSuggestion } = require("../../utils/checks");
 const { string } = require("../../utils/strings");
 module.exports = {
@@ -6,7 +6,7 @@ module.exports = {
 		name: "unfollow",
 		permission: 10,
 		aliases: ["unsubscribe", "unsub"],
-		usage: "unfollow [suggestion id]",
+		usage: "unfollow [suggestion id|auto]",
 		description: "Unfollows a suggestion",
 		enabled: true,
 		examples: "`{{p}}unfollow 123`\nUnfollows suggestion #123",
@@ -16,12 +16,19 @@ module.exports = {
 	do: async (locale, message, client, args) => {
 		let qUserDB = await dbQuery("User", { id: message.author.id });
 		if (!args[0]) return message.channel.send(string(locale, "INVALID_SUGGESTION_ID_ERROR", {}, "error"));
-		let [fetchSuggestion, qSuggestionDB] = await checkSuggestion(locale, message.guild, args[0]);
-		if (fetchSuggestion) return message.channel.send(fetchSuggestion);
-		if (!qUserDB.subscribed.find(s => s.id === qSuggestionDB.suggestionId)) return message.channel.send(string(locale, "NOT_FOLLOWING_ERROR", { id: qSuggestionDB.suggestionId }, "error"));
-		let index = qUserDB.subscribed.findIndex(s => s.id === qSuggestionDB.suggestionId);
-		qUserDB.subscribed.splice(index, 1);
-		qUserDB.save();
-		return message.channel.send(string(locale, "UNFOLLOW_SUCCESS", { id: qSuggestionDB.suggestionId }, "success"));
+		if (["auto", "automatic"].includes(args[0].toLowerCase())) {
+			if (!qUserDB.auto_subscribe) return message.channel.send(string(locale, "AUTOFOLLOW_ALREADY_DISABLED", {}, "error"));
+			qUserDB.auto_subscribe = false;
+			await dbModify("User", {id: qUserDB.id}, qUserDB);
+			return message.channel.send(string(locale, "AUTOFOLLOW_DISABLED", {}, "success"));
+		} else {
+			let [fetchSuggestion, qSuggestionDB] = await checkSuggestion(locale, message.guild, args[0]);
+			if (fetchSuggestion) return message.channel.send(fetchSuggestion);
+			if (!qUserDB.subscribed.find(s => s.id === qSuggestionDB.suggestionId)) return message.channel.send(string(locale, "NOT_FOLLOWING_ERROR", { id: qSuggestionDB.suggestionId }, "error"));
+			let index = qUserDB.subscribed.findIndex(s => s.id === qSuggestionDB.suggestionId);
+			qUserDB.subscribed.splice(index, 1);
+			qUserDB.save();
+			return message.channel.send(string(locale, "UNFOLLOW_SUCCESS", { id: qSuggestionDB.suggestionId }, "success"));
+		}
 	}
 };
