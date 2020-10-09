1644eea930c133abf15e9366114f62a5f6e1a646
BrightnessTM <khanolympics@gmail.com>
2020-10-09 09:36:19 -0400

    Fix commands channel

--- commands/suggestions/suggest.js
+++ commands/suggestions/suggest.js
@@ -52,9 +52,9 @@ module.exports = {
 			}
 		}
 
-		if ((qServerDB.config.channels.commands ? message.channel.id !== qServerDB.config.channels.commands : true) && (qServerDB.config.channels.commands_new.length > 0 ? !qServerDB.config.channels.commands_new.includes(message.channel.id) : false) && !noCommand) {
-			let channels = qServerDB.config.channels.commands_new;
-			if (qServerDB.config.channels.commands) channels.push(qServerDB.config.channels.commands);
+		let channels = qServerDB.config.channels.commands_new;
+		if (qServerDB.config.channels.commands) channels.push(qServerDB.config.channels.commands);
+		if (!channels.includes(message.channel.id) && !noCommand) {
 			return message.channel.send(string(locale, "SUBMIT_NOT_COMMAND_CHANNEL_ERROR", { channels: channels.map(c => `<#${c}>`).join(", ") }, "error"));
 		}
 
