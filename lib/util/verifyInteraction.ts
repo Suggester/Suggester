a0f25ae1d890dfd6e0d9e1efe318d48e335409f6
Ben Richeson <benricheson101@gmail.com>
2022-02-17 02:37:01 -0500

    feat: interaction verification

---
+++ lib/util/verifyInteraction.ts
@@ -0,0 +1,16 @@
+import {verify} from '@noble/ed25519';
+import {APIInteraction} from 'discord-api-types';
+
+export async function verifyInteraction(
+  pubkey: string,
+  payload: APIInteraction,
+  timestamp: string,
+  sig: string
+) {
+  const msg = Buffer.concat([
+    Buffer.from(timestamp, 'utf8'),
+    Buffer.from(JSON.stringify(payload), 'utf8'),
+  ]);
+
+  return verify(sig, msg, pubkey);
+}
